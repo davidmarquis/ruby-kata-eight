89c78ba6852cdb5acf7a0daa51fe406109f19d94
David Marquis <davidmarquis@MacBookPro.local>
2011-08-05 00:44:27 -0400

    Step 1 (readable and already quite fast)

---
+++ combinations_finder.rb
@@ -0,0 +1,60 @@
+class CombinationsFinder
+
+  MINIMUM_WORD_LENGTH = 2
+  MAXIMUM_WORD_LENGTH = 6
+
+  def initialize(dictionary)
+    @words = CombinationsFinder.sanitize(dictionary, MINIMUM_WORD_LENGTH, MAXIMUM_WORD_LENGTH)
+    @words_by_length = CombinationsFinder.group_words_by_length(@words, MAXIMUM_WORD_LENGTH)
+  end
+
+  def find_combinations()
+
+    result = []
+
+    for word in @words do
+
+      split_start_idx = MINIMUM_WORD_LENGTH - 1
+      split_end_idx = word.length - MINIMUM_WORD_LENGTH - 1
+
+      for split_idx in (split_start_idx..split_end_idx) do
+
+        split_left = word[0..split_idx]
+        split_right = word[split_idx + 1, word.length]
+
+        if word_exists?(split_left) and word_exists?(split_right)
+
+          result << "#{split_left} + #{split_right} => #{word}"
+
+        end
+
+      end
+    end
+
+    result
+  end
+
+  def word_exists?(word)
+    @words_by_length[word.length].include?(word)
+  end
+
+  def CombinationsFinder.sanitize(dictionary, min_length, max_length)
+    cleaned = dictionary.collect { |word| word.chomp }
+    cleaned.select { |word| word.length >= min_length and word.length <= max_length }
+  end
+
+  def CombinationsFinder.group_words_by_length(dictionary, max_length)
+    groups = initialize_groups_up_to(max_length)
+    for word in dictionary do
+      groups[word.length] << word
+    end
+    groups
+  end
+
+  def CombinationsFinder.initialize_groups_up_to(max_length)
+    groups = {}
+    for length in 1..max_length do groups[length] = [] end
+    groups
+  end
+
+end
---
+++ combinations_finder_spec.rb
@@ -0,0 +1,107 @@
+require "rspec"
+require './combinations_finder'
+
+describe CombinationsFinder do
+
+  describe "should find combinations of small words that make up bigger words from a dictionary of words loaded from the sample file" do
+
+    it "from a sample file" do
+
+      # Although not a proper test, this allows to proof the code against the provided fixtures.
+
+      file = File.new('wordlist.txt')
+      dictionary = file.readlines()
+
+      time = Time.now
+      finder = CombinationsFinder.new(dictionary)
+      matches = finder.find_combinations()
+      time = Time.now - time
+
+      printf "Executed in %s\n", (time)
+      puts matches
+    end
+
+    it "from example data" do
+
+      dictionary = [
+          'al',
+          'albums',
+          'aver',
+          'bar',
+          'barely',
+          'be',
+          'befoul',
+          'bums',
+          'by',
+          'con',
+          'convex',
+          'ely',
+          'foul',
+          'here',
+          'hereby',
+          'jig',
+          'jigsaw',
+          'or',
+          'saw',
+          'tail',
+          'tailor',
+          'vex',
+          'we',
+          'weaver'
+      ]
+
+      finder = CombinationsFinder.new(dictionary)
+      matches = finder.find_combinations()
+
+      matches.should == [
+        "al + bums => albums",
+        "bar + ely => barely",
+        "be + foul => befoul",
+        "con + vex => convex",
+        "here + by => hereby",
+        "jig + saw => jigsaw",
+        "tail + or => tailor",
+        "we + aver => weaver",
+      ]
+
+    end
+
+  end
+
+  # More granular testing
+
+  it "should only consider words of a certain length as valid" do
+
+    dictionary = ["short", "", "a", "verylongword"]
+
+    groups = CombinationsFinder.sanitize(dictionary, 2, 5)
+
+    groups.should == ["short"]
+
+  end
+
+  it "should only consider alpha character" do
+
+    dictionary = ["short    ", "    short", "short", "verylongword"]
+
+    groups = CombinationsFinder.sanitize(dictionary, 2, 5)
+
+    groups.should == ["short"]
+
+  end
+
+  it "should group words by length" do
+
+    dictionary = ['a', 'b', 'ab', 'abc', 'de']
+
+    groups = CombinationsFinder.group_words_by_length(dictionary, 4)
+
+    groups.should == {
+        1 => ['a', 'b'],
+        2 => ['ab', 'de'],
+        3 => ['abc'],
+        4 => [],
+    }
+  end
+
+end
